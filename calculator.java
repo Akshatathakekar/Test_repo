6397d9887aeae1ccac03365fab7214f8a082448a
Thakekar <akshata-shashikant.thakekar@capgemini.com>
2019-03-04 15:08:30 +0530

    removed statement

--- calculator.java
+++ calculator.java
@@ -1,12 +1,14 @@
 public class calculator
 {
 
-       public static void main(String args[])
-      {
- 
-      System.out.println("Hello World");
-
-      System.out.println("calculator java program");
-      }
+         public static void main(String args[])
+         {
+         System.out.println("Hello World");
+    
+          }
+	public static void addition(int number1,int number2)
+	{
+	System.out.println("Addition="+(number1+number2));
+	}
 
 }
