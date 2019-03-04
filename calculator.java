570a88861aca51efc543626661e55792e7f2f822
Thakekar <akshata-shashikant.thakekar@capgemini.com>
2019-03-04 15:12:29 +0530

    Revert "removed statement"
    This reverts commit 6397d9887aeae1ccac03365fab7214f8a082448a.

--- calculator.java
+++ calculator.java
@@ -1,14 +1,12 @@
 public class calculator
 {
 
-         public static void main(String args[])
-         {
-         System.out.println("Hello World");
-    
-          }
-	public static void addition(int number1,int number2)
-	{
-	System.out.println("Addition="+(number1+number2));
-	}
+       public static void main(String args[])
+      {
+ 
+      System.out.println("Hello World");
+
+      System.out.println("calculator java program");
+      }
 
 }
